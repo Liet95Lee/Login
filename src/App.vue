ba35c80539d67841900146217c181726cb50f5ad
LEE LEE GEAP <lietlee95@gmail.com>
2020-05-27 21:29:11 +0800

    基本的验证方法（表单验证）

--- src/App.vue
+++ src/App.vue
@@ -5,30 +5,75 @@
       <div class="layui-form-item">
         <label class="layui-form-label">用户名</label>
         <div class="layui-input-block">
-          <input type="text" name="title" required lay-verify="required" placeholder="请输入标题" autocomplete="off"
+          <input type="text" name="title" v-model="name" required lay-verify="required" placeholder="请输入标题" autocomplete="off"
             class="layui-input">
         </div>
       </div>
       <div class="layui-form-item">
         <label class="layui-form-label">密码</label>
         <div class="layui-input-block">
-          <input type="password" name="title" required lay-verify="required" placeholder="请输入标题" autocomplete="off"
+          <input type="password" name="title" v-model="password" required lay-verify="required" placeholder="请输入标题" autocomplete="off"
             class="layui-input">
         </div>
       </div>
       <div class="layui-form-item">
         <label class="layui-form-label">验证码</label>
-        <div class="layui-input-block">
-          <input type="text" name="title" required lay-verify="required" placeholder="请输入标题" autocomplete="off"
+        <div class="layui-input-inline">
+          <input type="text" name="title" v-model="code" required lay-verify="required" placeholder="请输入标题" autocomplete="off"
             class="layui-input">
         </div>
+        <div class="layui-form-mid svg" @click="getCaptcha()" v-html="svg"></div>
       </div>
-      <button type="button" class="layui-btn">点击登陆</button>
+      <button type="button" class="layui-btn" @click="checkForm()">点击登陆</button>
 <a href="http://www.layui.com" class="imooc-link">忘记密码</a>
     </form>
     </div>
   </div>
 </template>
+<script>
+import axios from 'axios'
+export default {
+  name: 'app',
+  data () {
+    return {
+      svg: '',
+      name: '',
+      password: '',
+      code: '',
+      errorMsg: []
+    }
+  },
+  mounted () {
+    this.getCaptcha()
+  },
+  methods: {
+    getCaptcha () {
+      axios.get('http://localhost:3000/getCaptcha').then((res) => {
+        if (res.status === 200) {
+          const obj = res.data
+          if (obj.code === 200) {
+            this.svg = obj.data
+          }
+        }
+      // console.log(res)
+      })
+    },
+    checkForm () {
+      this.errorMsg = []
+      // regEx email
+      if (!this.name) {
+        this.errorMsg.push('登陆名为空!')
+      }
+      if (!this.password) {
+        this.errorMsg.push('密码不得为空!')
+      }
+      if (!this.code) {
+        this.errorMsg.push('验证码为空!')
+      }
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 #app {
@@ -45,5 +90,9 @@ input {
   &:hover{
     color: #009688;
   }
+}
+.svg {
+  position: relative;
+  top: -15px;
 }
 </style>

b61727badaab1db86c2d7ecabb71a0d5c76c8cfe
LEE LEE GEAP <lietlee95@gmail.com>
2020-05-29 15:23:29 +0800

    VeeValidate 2x 的安装及使用（表单验证进阶）

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,29 @@
   <div id="app">
     <div class="layui-container">
     <form class="layui-form layui-form-pane" action="">
-      <div class="layui-form-item" :class="{ 'form-group--error': $v.name.$error }">
+      <div class="layui-form-item">
         <label class="layui-form-label">用户名</label>
         <div class="layui-input-inline">
-          <input type="text" name="title" v-model.trim="$v.name.$model" @input="setName($event.target.value)" placeholder="请输入标题" autocomplete="off"
+          <input type="text" name="name" v-model.trim="name" v-validate="'required|email'" placeholder="请输入标题" autocomplete="off"
             class="layui-input">
         </div>
-        <div class="error layui-form-mid" v-if="!$v.name.required">用户名不得为空</div>
-        <div class="error layui-form-mid" v-if="!$v.name.email">用户名输入格式错误</div>
+        <div class="error layui-form-mid">{{errors.first('name')}}</div>
       </div>
       <div class="layui-form-item">
         <label class="layui-form-label">密码</label>
-        <div class="layui-input-block">
-          <input type="password" name="title" v-model="password" placeholder="请输入标题" autocomplete="off"
+        <div class="layui-input-inline">
+          <input type="password" name="password" v-model="password" v-validate="'required|min:6'" placeholder="请输入标题" autocomplete="off"
             class="layui-input">
         </div>
+        <div class="error layui-form-mid">{{errors.first('password')}}</div>
       </div>
       <div class="layui-form-item">
         <label class="layui-form-label">验证码</label>
         <div class="layui-input-inline">
-          <input type="text" name="title" v-model="code" placeholder="请输入标题" autocomplete="off"
+          <input type="text" name="code" v-model="code" v-validate="'required|length:4'" placeholder="请输入标题" autocomplete="off"
             class="layui-input">
         </div>
+        <div class="error layui-form-mid">{{errors.first('code')}}</div>
         <div class="layui-form-mid svg" @click="getCaptcha()" v-html="svg"></div>
       </div>
       <button type="button" class="layui-btn" @click="checkForm()">点击登陆</button>
@@ -34,7 +35,6 @@
 </template>
 <script>
 import axios from 'axios'
-import { required, email } from 'vuelidate/lib/validators'
 export default {
   name: 'app',
   data () {
@@ -46,26 +46,10 @@ export default {
       errorMsg: []
     }
   },
-  validations: {
-    name: {
-      required,
-      email
-    },
-    password: {
-      required
-    },
-    code: {
-      required
-    }
-  },
   mounted () {
     this.getCaptcha()
   },
   methods: {
-    setName (value) {
-      this.name = value
-      this.$v.name.$touch()
-    },
     getCaptcha () {
       axios.get('http://localhost:3000/getCaptcha').then((res) => {
         if (res.status === 200) {
@@ -114,14 +98,7 @@ input {
   position: relative;
   top: -15px;
 }
-
 .error {
-  display: none;
-}
-
-.form-group--error {
-  .error {
-    display: block;
-  }
+  color: red;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vuelidate from 'vuelidate'
+import Veelidate, { Validator } from 'vee-validate'
+// method 1
+// import zh from 'vee-validate/dist/locale/zh_CN'
 
-Vue.use(Vuelidate)
+// method 2
+import './local/index'
+
+Vue.use(Veelidate)
+// method 1
+// Validator.localize('zh-CN', zh)
+
+const validator = new Validator()
+validator.localize('zh-CN')
 
 Vue.config.productionTip = false
 
